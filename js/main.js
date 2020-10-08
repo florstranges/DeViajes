fa2306a2b403e4ea36b2fd86214f46e7d5af1b1d
Florencia Stranges <florstranges@hotmail.com>
2020-10-07 22:04:35 -0300

    Agregando Jquery

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const dondeViajo = [
     {
         "pais": "Brasil",
         "destino": "Rio de Janeiro",
-        "precio_dia": 1293.4 
+        "precio_dia": 1293.4
     },
     {
         "pais": "Estados Unidos",
@@ -18,7 +18,7 @@ const dondeViajo = [
     {
         "pais": "Brasil",
         "destino": "Florianópolis",
-        "precio_dia": 1469 
+        "precio_dia": 1469
     },
     {
         "pais": "Estados Unidos",
@@ -32,14 +32,6 @@ const dondeViajo = [
     }
 ]
 
-// Convertir a JSON
-var jsonViaje = JSON.stringify(dondeViajo)
-
-// Transformo mi JSON a Objeto Arreglo
-var objetoJson = JSON.parse(jsonViaje)
-
-// Guardandolo en el storage
-localStorage.setItem('viaje', cargarDatos);
 
 // Constructor Viaje
 class viaje {
@@ -52,127 +44,158 @@ class viaje {
         this.categoria = categoria;
         this.precio = precio;
     }
-
-
 }
 
 document.addEventListener('DOMContentLoaded', cargarDatos);
-    //Cargando datos del viaje
-function cargarDatos(){
-        // Variales del proyecto
-        const selectPais = document.querySelector('#pais');
-        const selectDestino = document.querySelector('#destino');
-        const calendarioIda = document.querySelector('#start');
-        const calendarioVuelta = document.querySelector('#end');
-        const selectPasajeros = document.querySelector('#pasaj');
-        const selectCategoria = document.querySelector('#selectCategoria');
+//Cargando datos del viaje
+function cargarDatos() {
+    // Variales del proyecto
+    const selectPais = document.querySelector('#pais');
+    const selectDestino = document.querySelector('#destino');
+    const calendarioIda = document.querySelector('#start');
+    const calendarioVuelta = document.querySelector('#end');
+    const selectPasajeros = document.querySelector('#pasaj');
+    const selectCategoria = document.querySelector('#selectCategoria');
 
-        const calcularViaje = document.querySelector('#btnCalcular');
-        const resultadoViaje = document.querySelector('#listaViaje');
-        const sumaTotal = document.querySelector('#sumaTotal');
-        const comprar = document.querySelector('#btnComprarViaje');
+    const calcularViaje = document.querySelector('#btnCalcular');
+    const resultadoViaje = document.querySelector('#listaViaje');
+    const sumaTotal = document.querySelector('#sumaTotal');
 
+
+
+    const paises = crearListado(dondeViajo, "pais");
+
+    cargarSelect(paises, selectPais);
+
+    selectPais.addEventListener('change', function (e) {
+        selectDestino.innerHTML = '<option value=""> -- Seleccionar -- </opcion>';
+
+        const destinos = dondeViajo.filter(elem => elem.pais.toLocaleLowerCase().replace(' ', '-') == e.target.value);
+
+        const listaDestinos = crearListado(destinos, "destino");
+
+        cargarSelect(listaDestinos, selectDestino)
+    });
+
+
+    calcularViaje.addEventListener('focus', calcularDatos);
+
+    function calcularDatos(e) {
+        e.preventDefault();
         
-        const paises = crearListado(dondeViajo, "pais");
+        // Leer Pais Elegido
+        var paisEleg = selectPais.value;
+        // Leer Destino Elegido
+        var destinoEleg = selectDestino.value;
+        // Leer Fecha elegida
+        var fechaIdaEleg = calendarioIda.value;
+        var fechaVueltaEleg = calendarioVuelta.value;
+        // Leer Cantidad de pasajeros
+        var cantidadPasajeros = selectPasajeros.value;
 
-        cargarSelect(paises, selectPais);
 
-        selectPais.addEventListener('change', function (e){
-            selectDestino.innerHTML = '<option value=""> -- Seleccionar -- </opcion>';
 
-            const destinos = dondeViajo.filter(elem => elem.pais.toLocaleLowerCase().replace(' ', '-') == e.target.value);
-
-            const listaDestinos = crearListado(destinos, "destino");
-
-            cargarSelect(listaDestinos, selectDestino)
-        });
-
-        calcularViaje.addEventListener('click', calcularDatos);
-        
-        function calcularDatos(e){
-            e.preventDefault();
-            // Leer Pais Elegido
-            var paisEleg = selectPais.value;
-            // Leer Destino Elegido
-            var destinoEleg = selectDestino.value;
-            // Leer Fecha elegida
-            var fechaIdaEleg = calendarioIda.value;
-            var fechaVueltaEleg = calendarioVuelta.value;
-            // Leer Cantidad de pasajeros
-            var cantidadPasajeros = selectPasajeros.value;
-            
-
-            
-            // Cantidad dias de viaje
-            function restaFechas(f1,f2) {
-                var fFecha1 = new Date(calendarioIda.value);
-                var fFecha2 = new Date(calendarioVuelta.value);
-                var dif = fFecha2 - fFecha1;
-                var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
-                return dias;
-            }
-            var diferenciaDias = restaFechas(calendarioIda.value, calendarioVuelta.value);
-
-            // Buscar los precios
-            precioDia = dondeViajo.find(elem => elem.destino.toLowerCase().replace(' ', '-') == destinoEleg)
-            const precio_x_dia = precioDia.precio_dia;
-
-            // Eligiendo la categoria
-            var categEleg = selectCategoria.value;
-            if(categEleg == "Business"){
-                var categoriaElegida = 1.2 
-            } else {
-                var categoriaElegida = 1
-            }
-
-            // Calculando Total
-            var totalPagar = Math.round((precio_x_dia * diferenciaDias) * (cantidadPasajeros) * (categoriaElegida));
-
-            // Creando un array para el resumen
-            var listadoSeleccionado = [];
-
-            listadoSeleccionado.push("PAIS: " + paisEleg);
-            listadoSeleccionado.push("DESTINO: " + destinoEleg);
-            listadoSeleccionado.push("FECHA DE IDA: " + fechaIdaEleg);
-            listadoSeleccionado.push("FECHA DE VUELTA: " + fechaVueltaEleg);
-            listadoSeleccionado.push("CANTIDAD DE PASAJEROS: " + cantidadPasajeros);
-            listadoSeleccionado.push("CATEGORIA: " + categEleg);
-
-            // Cargando resumen en el HTML 
-            resultadoViaje.innerHTML = ''
-            resultadoViaje.classList.add('resumen');
-            for(var i = 0; i < listadoSeleccionado.length; i++){
-                resultadoViaje.innerHTML += listadoSeleccionado[i] + '</br>';
-            }
-
-            // Cargar total en HTML
-            sumaTotal.innerHTML = "$ " + totalPagar;
-            sumaTotal.classList.add('sumatotal');
-
+        // Cantidad dias de viaje
+        function restaFechas(f1, f2) {
+            var fFecha1 = new Date(calendarioIda.value);
+            var fFecha2 = new Date(calendarioVuelta.value);
+            var dif = fFecha2 - fFecha1;
+            var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
+            return dias;
         }
+        var diferenciaDias = restaFechas(calendarioIda.value, calendarioVuelta.value);
+
+        // Buscar los precios
+        precioDia = dondeViajo.find(elem => elem.destino.toLowerCase().replace(' ', '-') == destinoEleg)
+        const precio_x_dia = precioDia.precio_dia;
+
+        // Eligiendo la categoria
+        var categEleg = selectCategoria.value;
+        if (categEleg == "Business") {
+            var categoriaElegida = 1.2
+        } else {
+            var categoriaElegida = 1
+        }
+
+        // Calculando Total
+        var totalPagar = Math.round((precio_x_dia * diferenciaDias) * (cantidadPasajeros) * (categoriaElegida));
+
+        // Creando un array para el resumen
+        var listadoSeleccionado = [];
+
+        listadoSeleccionado.push("PAIS: " + paisEleg);
+        listadoSeleccionado.push("DESTINO: " + destinoEleg);
+        listadoSeleccionado.push("FECHA DE IDA: " + fechaIdaEleg);
+        listadoSeleccionado.push("FECHA DE VUELTA: " + fechaVueltaEleg);
+        listadoSeleccionado.push("CANTIDAD DE PASAJEROS: " + cantidadPasajeros);
+        listadoSeleccionado.push("CATEGORIA: " + categEleg);
+
+        // Cargando resumen en el HTML 
+        resultadoViaje.innerHTML = ''
+        resultadoViaje.classList.add('resumen');
+        for (var i = 0; i < listadoSeleccionado.length; i++) {
+            resultadoViaje.innerHTML += listadoSeleccionado[i] + '</br>';
+        }
+
+        // Cargar total en HTML
+        sumaTotal.innerHTML = "$ " + totalPagar;
+        sumaTotal.classList.add('sumatotal');
+
+        // Abre y cierra menu desplegable
+        (function($) {
+            "use strict";
+            var openBtn = $("#btnCalcular"),
+                closeBtn = $("#close-button"),
+                menu = $(".menu-wrap");
+            // Open menu when click on menu button
+            openBtn.on("click", function() {
+                menu.addClass("active");
+            });
+            // Close menu when click on Close button
+            closeBtn.on("click", function() {
+                menu.removeClass("active");
+            });
+        })(jQuery);
+
+        var nuevoViaje = new viaje(paisEleg, destinoEleg, fechaIdaEleg, fechaVueltaEleg, cantidadPasajeros, categEleg, totalPagar);
+        localStorage.setItem('viaje', JSON.stringify(nuevoViaje));
+        
+    }
 };
-
-
 
 
 // Genero listado para los select
 function crearListado(array, parametro) {
-	const listado = [];
+    const listado = [];
 
-	array.forEach(elemento => {
-		if (!listado.includes(elemento[parametro])) {
-			listado.push(elemento[parametro]);
-		}
-	})
-	return listado.sort();
+    array.forEach(elemento => {
+        if (!listado.includes(elemento[parametro])) {
+            listado.push(elemento[parametro]);
+        }
+    })
+    return listado.sort();
 }
 
 // Cargo contenido en los Select
 function cargarSelect(array, select) {
-	array.forEach(element => {
-		let option = document.createElement('option');
-		option.value = element.toLowerCase().replace(' ', '-');
-		option.textContent = element;
-		select.appendChild(option);
-	})
+    array.forEach(element => {
+        let option = document.createElement('option');
+        option.value = element.toLowerCase().replace(' ', '-');
+        option.textContent = element;
+        select.appendChild(option);
+    })
+}
+
+var botonBrasil = document.querySelector('#botonBrasil');
+botonBrasil.addEventListener('click', paquetes('Brasil', 'Rio de Janeiro'));
+
+
+function paquetes(p, d, fI, fV, pA, c, pR){
+    selectPais = p;
+    selectDestino = d;
+    calendarioIda = fI;
+    calendarioVuelta = fV;
+    selectPasajeros = pA;
+    selectCategoria = c;
+    /*pr*/
 }
