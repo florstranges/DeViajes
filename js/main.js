d9623ec57625e003a1146af434e5288b18704d27
Florencia Stranges <florstranges@hotmail.com>
2020-10-13 18:36:54 -0300

    Tarjeta de credito

--- js/main.js
+++ js/main.js
@@ -82,7 +82,7 @@ function cargarDatos() {
 
     function calcularDatos(e) {
         e.preventDefault();
-        
+
         // Leer Pais Elegido
         var paisEleg = selectPais.value;
         // Leer Destino Elegido
@@ -141,27 +141,180 @@ function cargarDatos() {
         sumaTotal.innerHTML = "$ " + totalPagar;
         sumaTotal.classList.add('sumatotal');
 
+
         // Abre y cierra menu desplegable
-        (function($) {
+        (function ($) {
             "use strict";
             var openBtn = $("#btnCalcular"),
                 closeBtn = $("#close-button"),
                 menu = $(".menu-wrap");
             // Open menu when click on menu button
-            openBtn.on("click", function() {
+            openBtn.on("click", function () {
                 menu.addClass("active");
             });
             // Close menu when click on Close button
-            closeBtn.on("click", function() {
+            closeBtn.on("click", function () {
                 menu.removeClass("active");
             });
         })(jQuery);
 
         var nuevoViaje = new viaje(paisEleg, destinoEleg, fechaIdaEleg, fechaVueltaEleg, cantidadPasajeros, categEleg, totalPagar);
         localStorage.setItem('viaje', JSON.stringify(nuevoViaje));
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Modal pago y TC
+        const tarjeta = document.querySelector('#tarjeta'),
+            btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+            formulario = document.querySelector('#formulario-tarjeta'),
+            numeroTarjeta = document.querySelector('#tarjeta .numero'),
+            nombreTarjeta = document.querySelector('#tarjeta .nombre'),
+            logoMarca = document.querySelector('#logo-marca'),
+            firma = document.querySelector('#tarjeta .firma p'),
+            mesExpiracion = document.querySelector('#tarjeta .mes'),
+            yearExpiracion = document.querySelector('#tarjeta .year'),
+            modalContent = document.querySelector('#modal-content');
+        ccv = document.querySelector('#tarjeta .ccv');
+
+        // * Volteamos la tarjeta para mostrar el frente.
+        const mostrarFrente = () => {
+            if (tarjeta.classList.contains('active')) {
+                tarjeta.classList.remove('active');
+            }
+        }
+
+        // * Rotacion de la tarjeta
+        tarjeta.addEventListener('click', () => {
+            tarjeta.classList.toggle('active');
+        });
+
+        // * Boton de abrir formulario
+        btnAbrirFormulario.addEventListener('click', () => {
+            btnAbrirFormulario.classList.toggle('active');
+            formulario.classList.toggle('active');
+            modalContent.classList.toggle('active');
+        });
+
+        // * Select del mes generado dinamicamente.
+        for (let i = 1; i <= 12; i++) {
+            let opcion = document.createElement('option');
+            opcion.value = i;
+            opcion.innerText = i;
+            formulario.selectMes.appendChild(opcion);
+        }
+
+        // * Select del año generado dinamicamente.
+        const yearActual = new Date().getFullYear();
+        for (let i = yearActual; i <= yearActual + 8; i++) {
+            let opcion = document.createElement('option');
+            opcion.value = i;
+            opcion.innerText = i;
+            formulario.selectYear.appendChild(opcion);
+        }
+
+        // * Input numero de tarjeta
+        formulario.inputNumero.addEventListener('keyup', (e) => {
+            let valorInput = e.target.value;
+
+            formulario.inputNumero.value = valorInput
+                // Eliminamos espacios en blanco
+                .replace(/\s/g, '')
+                // Eliminar las letras
+                .replace(/\D/g, '')
+                // Ponemos espacio cada cuatro numeros
+                .replace(/([0-9]{4})/g, '$1 ')
+                // Elimina el ultimo espaciado
+                .trim();
+
+            numeroTarjeta.textContent = valorInput;
+
+            if (valorInput == '') {
+                numeroTarjeta.textContent = '#### #### #### ####';
+
+                logoMarca.innerHTML = '';
+            }
+
+            if (valorInput[0] == 4) {
+                logoMarca.innerHTML = '';
+                const imagen = document.createElement('img');
+                imagen.src = 'multimedia/visa.png';
+                logoMarca.appendChild(imagen);
+            } else if (valorInput[0] == 5) {
+                logoMarca.innerHTML = '';
+                const imagen = document.createElement('img');
+                imagen.src = 'multimedia/mastercard.png';
+                logoMarca.appendChild(imagen);
+            }
+
+            // Volteamos la tarjeta para que el usuario vea el frente.
+            mostrarFrente();
+        });
+
+        // * Input nombre de tarjeta
+        formulario.inputNombre.addEventListener('keyup', (e) => {
+            let valorInput = e.target.value;
+
+            formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+            nombreTarjeta.textContent = valorInput;
+            firma.textContent = valorInput;
+
+            if (valorInput == '') {
+                nombreTarjeta.textContent = 'Jhon Doe';
+            }
+
+            mostrarFrente();
+        });
+
+        // * Select mes
+        formulario.selectMes.addEventListener('change', (e) => {
+            mesExpiracion.textContent = e.target.value;
+            mostrarFrente();
+        });
+
+        // * Select Año
+        formulario.selectYear.addEventListener('change', (e) => {
+            yearExpiracion.textContent = e.target.value.slice(2);
+            mostrarFrente();
+        });
+
+        // * CCV
+        formulario.inputCCV.addEventListener('keyup', () => {
+            if (!tarjeta.classList.contains('active')) {
+                tarjeta.classList.toggle('active');
+            }
+
+            formulario.inputCCV.value = formulario.inputCCV.value
+                // Eliminar los espacios
+                .replace(/\s/g, '')
+                // Eliminar las letras
+                .replace(/\D/g, '');
+
+            ccv.textContent = formulario.inputCCV.value;
+        });
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        const botonFinalizar = document.querySelector('#btn-finalizar');
         
+        botonFinalizar.addEventListener('click', function () {
+            const div = document.createElement('div');
+            div.classList.add('mt-10', 'text-left');
+
+            div.innerHTML = `
+                <p>Compra exitosa</p>
+                `;
+
+            const resultadoTajeta = document.querySelector('#resultado');
+            // Mostrar el spinner
+            const spinner = document.querySelector('#cargando');
+            spinner.style.display = 'flex';
+
+            setTimeout(() => {
+                spinner.style.display = 'none';
+                resultadoTajeta.appendChild(div);
+            }, 3000);
+        })
     }
-};
+}
 
 
 // Genero listado para los select
@@ -186,7 +339,8 @@ function cargarSelect(array, select) {
     })
 }
 
-var botonBrasil = document.querySelector('#botonBrasil');
+
+/* var botonBrasil = document.querySelector('#botonBrasil');
 botonBrasil.addEventListener('click', paquetes('Brasil', 'Rio de Janeiro'));
 
 
@@ -197,5 +351,6 @@ function paquetes(p, d, fI, fV, pA, c, pR){
     calendarioVuelta = fV;
     selectPasajeros = pA;
     selectCategoria = c;
-    /*pr*/
+    pR
 }
+*/
